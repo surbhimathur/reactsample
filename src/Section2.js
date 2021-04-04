f3535d48120fab79e971089826943abb942f0912
surbhi <surbhimathur9@gmail.com>
2021-04-04 23:32:15 +0530

    responsive complete

--- src/Section2.js
+++ src/Section2.js
@@ -1,24 +1,24 @@
-import'../src/Section2.css';
+import '../src/Section2.css';
 
 import Photo1 from '../src/images/photo1.jpg';
 import Photo2 from '../src/images/photo2.jpg';
 import Photo3 from '../src/images/photo3.jpg';
 import React from 'react';
 
-function Section2(){
-    return(
+function Section2() {
+    return (
         <section>
             <div className='features'>
-             <div className='features-text'>
-             <p>The journey of a thousand miles begins with a single step.<br></br>
+                <div className='features-text'>
+                    <p>The journey of a thousand miles begins with a single step.<br></br>
              Collect Moments, Not Things.</p>
-             </div>
-             
-             <div className='features-images'>
-               <img src={Photo1} alt='travel'></img><button className='travel-btn'>Travel</button>
-               <img src={Photo2} alt='travel'></img><button className='eat-btn'>Eat</button>
-               <img src={Photo3} alt='travel'></img><button className='relax-btn'>Relax</button>
-             </div>
+                </div>
+
+                <div className='features-images'>
+               <div><img src={Photo1} alt='travel'></img><button className='travel-btn'>Travel</button></div> 
+                 <div><img src={Photo2} alt='travel'></img><button className='eat-btn'>Eat</button></div>  
+                 <div><img src={Photo3} alt='travel'></img><button className='relax-btn'>Relax</button></div>   
+                </div>
 
             </div>
 
@@ -26,15 +26,15 @@ function Section2(){
                 <div ><p>As featured in</p></div>
                 <div><p><span id="boldtravel">travel</span><br></br>&nbsp; &nbsp; &nbsp;blog</p></div>
                 <div><p class="airplane"><i class='fas fa-plane'></i></p>
-                <p id="planet">PLANET <span id="boldtour">TOUR</span></p></div>
+                    <p id="planet">PLANET <span id="boldtour">TOUR</span></p></div>
                 <div><p id="world">WORLD<br></br>TRAVEL</p></div>
                 <div><p class="cloud"><i class='fas fa-cloud sky'></i></p>
-                <p id="sky">skycloud</p></div>
+                    <p id="sky">skycloud</p></div>
             </div>
         </section>
     )
-    
 
-    
+
+
 }
 export default Section2;
